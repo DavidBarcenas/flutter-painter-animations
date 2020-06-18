2375ab7afecaa455424829f56283463fe9527712
David Barcenas <daveepro@outlook.com>
2020-06-18 13:43:23 -0500

    feat: Border radius header

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -9,3 +9,17 @@ class SquareHeader extends StatelessWidget {
     );
   }
 }
+
+class BorderRadiusHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      decoration: BoxDecoration(
+        color: Color(0xff615aab),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(70), bottomRight: Radius.circular(70))
+      ),
+    );
+  }
+}
+
