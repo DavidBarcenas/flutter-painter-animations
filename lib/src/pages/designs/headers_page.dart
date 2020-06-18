25df32b0d9ba5e1a33f17aca05cbf4d819ce3e27
David Barcenas <daveepro@outlook.com>
2020-06-18 11:57:30 -0500

    feat: Square header

---
+++ lib/src/pages/designs/headers_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:custom_painter/src/widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SquareHeader()
+    );
+  }
+}
---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SquareHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      color: Color(0xff615aab)
+    );
+  }
+}
